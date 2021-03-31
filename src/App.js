27e83c68bf60fbf4b24e85d343ed6962541c3170
Tim Dayley <tdayley@gmail.com>
2021-03-31 16:49:16 -0700

    add Body component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
+import './css/main.css';
 import Header from './components/Header'
+import Body from './components/Body'
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
     <>
       <Header />
-      <div class="wrapper">
-			  body
-		  </div>	
+      <Body />
     </>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Body = () => {
+    return (
+        <div className="wrapper">
+			  body
+		  </div>
+    )
+}
+
+export default Body
