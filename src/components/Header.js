556c6a29aa65154d4e2766321ad93e6c1a55a570
Tim Dayley <tdayley@gmail.com>
2021-03-31 16:48:41 -0700

    change signature and update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,11 @@
-import signature from '../images/signature_v1.svg'
-import largePortrait from '../images/portrait_v4_large.gif'
-import mediumPortrait from '../images/portrait_v4_medium.gif'
-import smallPortrait from '../images/portrait_v4_small.gif'
+import { Icon } from 'semantic-ui-react'
+import {ReactComponent as Signature } from '../images/signature.svg'
 
 const Header = () => {
     return (
-        <div class="fill">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        navbar
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                        <img alt='timdayley' src={signature} width='650' />
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                        <img class="portrait" src={smallPortrait} 
-                            srcset={`${largePortrait} 1024w, ${mediumPortrait} 640w, ${smallPortrait} 320w`} alt="human" />
-                    </div>
-                </div>
-            </div>
+        <div className="fill">
+            <Signature />
+            <Icon name='arrow down' size='big' className='splash-arrow' />
         </div>
     )
 }
