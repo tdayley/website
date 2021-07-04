d101fc6c4a306f8d2eed8a752ca432d110d615e4
Tim Dayley <tdayley@gmail.com>
2021-07-03 19:32:01 -0700

    clean up projects, adjust styling

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,94 +1,55 @@
 import Title from './Title';
-import { Container, Segment } from 'semantic-ui-react';
+import { Container, Item, Label } from 'semantic-ui-react';
 import { ReactComponent as PebbleWatchface } from '../images/pebble.svg';
+import areTheyGoodLogo from '../images/aretheygoodlogo.png'
 
 const Body = () => {
 	return (
 		<div className="wrapper">
-			<Container text className="margin-bottom-50">
+			<Container text textAlign='justified' className="margin-bottom-50">
 				<Title name="about" iconName="info circle" />
-				<p>
-					Hello! My name is Tim and you are on my website. Since this is the
-					about section, I guess I should let you know that I am a web developer
-					with a passion for creating cutting-edge sites and stunning designs.
-					When I am away from the computer, you can either find me enjoying the
-					outdoors or getting back on the computer. Feel free to send{' '}
-					<a href="mailto:tdayley@gmail.com">me</a> an email should you have any
-					questions.
+				<p className="section-paragraph">
+					A web developer	with a passion for creating cutting-edge sites and stunning designs. When away from the computer, you can either find me enjoying the outdoors kayaking or planning my next hike. Feel free to send <a href="mailto:tdayley@gmail.com">me</a> an email should you have any questions or want to connect!
 				</p>
 			</Container>
-			<Container text className="margin-bottom-50">
+			<Container text textAlign='justified' className="margin-bottom-50">
 				<Title name="projects" iconName="clipboard list" />
-				<Container>
-					<Segment>
-						<PebbleWatchface
-							href="https://apps.getpebble.com/en_US/application/55ac922477eb5ef7ab000048?section=watchfaces"
-							target="_blank"
-						/>
-						{/* <div id="svg-container">
-                            <a href="https://apps.getpebble.com/en_US/application/55ac922477eb5ef7ab000048?section=watchfaces" target="_blank">
-                                <PebbleWatchface className='' />
-                            </a>
-                        </div> */}
-						<h3>
-							<a
-								href="https://apps.getpebble.com/en_US/application/55ac922477eb5ef7ab000048?section=watchfaces"
-								target="_blank"
-								rel="noreferrer"
-							>
+				<Item.Group>
+					<Item>
+						<Item.Image size="medium">
+							<PebbleWatchface />
+						</Item.Image>
+						<Item.Content>
+							<Item.Header as="a" href="https://github.com/tdayley/Pebble" target="_blank" rel="noreferrer">
 								Pebble Watchface
-							</a>
-						</h3>
-						<p className="section-paragraph">
-							A Pebble watchface for the original Pebble Steel. It uses the
-							<a
-								href="https://openweathermap.org/"
-								target="_blank"
-								rel="noreferrer"
-								className="clearly-a-link"
-							>
-								OpenWeatherMap API
-							</a>{' '}
-							to display an up to date (synced every 5 minutes) temperature
-							right on your watchface. Also created a custom time display that
-							shows the date, time, and battery.
-						</p>
-					</Segment>
-				</Container>
-				<Container>
-					<div>
-						<h3>
-							<a
-								href="http://aretheygood.timdayley.com"
-								target="_blank"
-								rel="noreferrer"
-							>
+							</Item.Header>
+							<Item.Extra>
+								<Label icon="ban" content="deprecated" />
+							</Item.Extra>
+							<Item.Description>
+								<p className="section-paragraph">
+									A Pebble watchface for the original Pebble Steel. It uses the <a href="https://openweathermap.org/" target="_blank" rel="noreferrer" className="clearly-a-link">OpenWeatherMap API</a> to display an up to date (synced every 5 minutes) temperature right on your watchface. Also created a custom time display that shows the date, time, and battery. This is a lot of random text that I am using to test whether adding a lot of text would make this set up look weird or not.
+								</p>
+							</Item.Description>
+						</Item.Content>
+					</Item>
+					<Item>
+						<Item.Image src={areTheyGoodLogo} size="medium" alt="Are they good..." circular />
+						<Item.Content>
+							<Item.Header as="a" href="https://github.com/tdayley/AreTheyGood" target="_blank" rel="noreferrer">
 								Are they good...
-							</a>
-						</h3>
-						<p className="section-paragraph">
-							This is a project I created for the RIOT Games API 2016 Challenge.
-							It uses the
-							<a
-								href="https://developer.riotgames.com/"
-								target="_blank"
-								rel="noreferrer"
-								className="clearly-a-link"
-							>
-								League of Legends API
-							</a>{' '}
-							to grab information of a summoner and give detailed information of
-							their best champions.
-						</p>
-					</div>
-					<div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-						<img
-							className="project-image"
-							src="../images/aretheygoodlogo.png"
-							alt="Are they good..."
-						/>
-					</div>
-				</Container>
+							</Item.Header>
+							<Item.Extra>
+								<Label icon="ban" content="deprecated" />
+							</Item.Extra>
+							<Item.Description>
+								<p className="section-paragraph">
+									This is a project I created for the RIOT Games API 2016 Challenge. It uses the <a href="https://developer.riotgames.com/" target="_blank" rel="noreferrer" className="clearly-a-link" >League of Legends API</a> to grab information of a summoner and give detailed information of their best champions.
+								</p>
+							</Item.Description>
+						</Item.Content>
+					</Item>
+				</Item.Group>
 			</Container>
 			<Container text className="margin-bottom-50">
 				<Title name="contact" iconName="mail" />
