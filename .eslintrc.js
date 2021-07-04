2def8d2cc69566d25984bcb8fb7345c7f8c9ab26
Tim Dayley <tdayley@gmail.com>
2021-07-03 17:56:30 -0700

    fix issue with rules in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-        "@react/react-in-jsx-scope": "off",
+        "react/react-in-jsx-scope": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
     },
     settings: {
